c57748878e95e61f31167faa1d70802aa36392f9
Gonzalo Villegas Curulla <gonzalo.villegas@hotmail.com>
2020-04-13 11:02:36 +0200

    Deleted old plots, added legends.

--- template.m
+++ template.m
@@ -5,6 +5,11 @@ delete time_series_covid19_confirmed_global.csv
 
 % url = ['https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'];
 
+% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+%   Acquire data
+% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
 url = ['https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'];
 
 filename = 'time_series_covid19_confirmed_global.csv';
@@ -12,11 +17,15 @@ options = weboptions('TimeOut',Inf);
 outputfilename = websave(filename, url, options)
 
 data = readtable('time_series_covid19_confirmed_global.csv');
-% data = readtable('time_series_19-covid-Confirmed_global.csv'); %
-% Deprecated
 
-country = [18 20 161 408];
+% For deprecated older files only
+% country = [18 20 161 408]; % [It, Esp, Fr, UK]
 
+% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+%   Prepare series
+% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+% Convert data
 LL = size(data); LL = LL(2);
 
 temp = data(1,5:LL);
@@ -40,35 +49,11 @@ france  = cellfun(@str2num,france); france = france';
 italy = cellfun(@str2num,italy); italy = italy';
 uk = cellfun(@str2num,uk); uk = uk';
 
-figure,
-plot(france(2:end)./france(1:end-1), 'linewidth',1),
-% ylim([0 7]),
-grid on, hold on
-plot(spain(2:end)./spain(1:end-1), 'linewidth',1),
-plot(italy(2:end)./italy(1:end-1), 'linewidth',1),
-plot(uk(2:end)./uk(1:end-1), 'linewidth',1),
 
-% figure,
-% plot((france(3:end)-france(2:end-1))./(france(2:end-1)-france(1:end-2)), 'linewidth',1)
-% ylim([0 7]), grid on, hold on
-% plot((spain(3:end)-spain(2:end-1))./(spain(2:end-1)-spain(1:end-2)), 'linewidth',1)
-% plot((italy(3:end)-italy(2:end-1))./(italy(2:end-1)-italy(1:end-2)), 'linewidth',1)
-% plot((uk(3:end)-uk(2:end-1))./(uk(2:end-1)-uk(1:end-2)), 'linewidth',1)
-legend('france','spain','italy','uk');
-%%%%%%%%%%%%
-figure
-subplot(221), plot(spain), yyaxis right, plot(gradient(spain)), hold on, plot(del2(spain)), legend('spain','grad','del2','location','northwest'), grid on
-title('Total confirmed cases');
-subplot(222), plot(italy), yyaxis right, plot(gradient(italy)), hold on, plot(del2(italy)), legend('italy','grad','del2','location','northwest'), grid on
-subplot(223), plot(france), yyaxis right, plot(gradient(france)), hold on, plot(del2(france)), legend('france','grad','del2','location','northwest'), grid on
-subplot(224), plot(uk), yyaxis right, plot(gradient(uk)), hold on, plot(del2(uk)), legend('uk','grad','del2','location','northwest'), grid on
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% New cases
+% Create time series
 
 ESP = [0; spain(2:end)-spain(1:end-1)];
-ESPor = ESP;
+ESPor = ESP; % -or termination for new cases series without 5-day moving average
 ESP(5:end) = (ESP(5:end) +ESP(4:end-1) +ESP(3:end-2) +ESP(2:end-3) +ESP(1:end-4) )/5; ESP = [0;0;0;0; ESP];
 
 IT  = [0; italy(2:end)-italy(1:end-1)];
@@ -83,17 +68,47 @@ UK  = [0; uk(2:end)-uk(1:end-1)];
 UKor = UK;
 UK(5:end) = (UK(5:end) +UK(4:end-1) +UK(3:end-2) +UK(2:end-3) +UK(1:end-4) )/5; UK = [0;0;0;0; UK];
 
-figure
-subplot(221), yyaxis left, plot(ESPor), hold on, plot(ESP), ylim([-0.66*max(ESP) 1.05*max(ESP)]);
-    yyaxis right, plot(gradient(ESP)), hold on, plot(del2(ESP)), legend('spain','5dayav','grad','del2','location','northwest'), grid on, ylim([1.05*min(min(gradient(ESP)),min(del2(ESP))), 2.5*max(max(gradient(ESP)),max(del2(ESP)))]);
-title('Confirmed new cases');
-
-subplot(222), yyaxis left, plot(ITor), hold on, plot(IT), ylim([-0.66*max(IT) 1.05*max(IT)]);
-    yyaxis right, plot(gradient(IT)), hold on, plot(del2(IT)), legend('italy','5dayav','grad','del2','location','northwest'), grid on, ylim([1.05*min(min(gradient(IT)),min(del2(IT))), 2.5*max(max(gradient(IT)),max(del2(IT)))]);
-
-subplot(223), yyaxis left, plot(FRor), hold on, plot(FR), ylim([-0.66*max(FR) 1.05*max(FR)]);
-    yyaxis right, plot(gradient(FR)), hold on, plot(del2(FR)), legend('france','5dayav','grad','del2','location','northwest'), grid on, ylim([1.05*min(min(gradient(FR)),min(del2(FR))), 2.5*max(max(gradient(FR)),max(del2(FR)))]);
+% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+%   Plot new cases
+% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
-subplot(224), yyaxis left, plot(UKor), hold on, plot(UK), ylim([-0.66*max(UK) 1.05*max(UK)]);
-    yyaxis right, plot(gradient(UK)), hold on, plot(del2(UK)), legend('uk','5dayav','grad','del2','location','northwest'), grid on, ylim([1.05*min(min(gradient(UK)),min(del2(UK))), 2.5*max(max(gradient(UK)),max(del2(UK)))]);
+fig1 = figure('Name','Confirmed new cases','Units','Normalized', 'OuterPosition',[0 0 1 1]);
+
+% Spain
+subplot(221), yyaxis left, plot(ESPor), hold on, plot(ESP),
+    ylim([-0.66*max(ESPor) 1.05*max(ESPor)]);
+    yyaxis right, plot(gradient(ESP)), hold on, plot(del2(ESP)),
+    grid on, ylim([1.05*min(min(gradient(ESP)),min(del2(ESP))), 2.5*max(max(gradient(ESP)),max(del2(ESP)))]);
+    lgnd1 = legend('New cases','5-day-av','grad','del2','location','northwest');
+    title(lgnd1,'Spain');
+
+% Italy
+subplot(222), yyaxis left, plot(ITor), hold on, plot(IT),
+    ylim([-0.66*max(ITor) 1.05*max(ITor)]);
+    yyaxis right, plot(gradient(IT)), hold on, plot(del2(IT)),
+    grid on, ylim([1.05*min(min(gradient(IT)),min(del2(IT))), 2.5*max(max(gradient(IT)),max(del2(IT)))]);
+    lgnd2 = legend('New cases','5-day-av.','grad','del2','location','northwest');
+    title(lgnd2,'Italy');
+
+% France
+subplot(223), yyaxis left, plot(FRor), hold on, plot(FR),
+    ylim([-0.66*max(FRor) 1.05*max(FRor)]);
+    yyaxis right, plot(gradient(FR)), hold on, plot(del2(FR)),
+    grid on, ylim([1.05*min(min(gradient(FR)),min(del2(FR))), 2.5*max(max(gradient(FR)),max(del2(FR)))]);
+    lgnd3 = legend('france','5-day-av','grad','del2','location','northwest');
+    title(lgnd3,'France');
+
+% United Kingdom
+subplot(224), yyaxis left, plot(UKor), hold on, plot(UK),
+    ylim([-0.66*max(UKor) 1.05*max(UKor)]);
+    yyaxis right, plot(gradient(UK)), hold on, plot(del2(UK)),
+    grid on, ylim([1.05*min(min(gradient(UK)),min(del2(UK))), 2.5*max(max(gradient(UK)),max(del2(UK)))]);
+    lgnd4 = legend('uk','5-day-av','grad','del2','location','northwest');
+    title(lgnd4, 'United Kingdom');
+
+% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+%   Save plots
+% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+%
